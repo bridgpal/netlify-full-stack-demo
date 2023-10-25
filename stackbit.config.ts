c906e46abe4a72784372677bf28d5eb310e02737
Anil Bridgpal <bridgpal@gmail.com>
2023-10-24 22:57:24 -0400

    stackbit config 2

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,17 +1,18 @@
-import { defineStackbitConfig } from '@stackbit/types';
-import { ContentfulContentSource } from '@stackbit/cms-contentful'
+import { ContentfulContentSource } from '@stackbit/cms-contentful';
 
-export default defineStackbitConfig({
+const config = {
   stackbitVersion: '~0.6.0',
   ssgName: 'nextjs',
   nodeVersion: '16',
   contentSources: [
     new ContentfulContentSource({
-      spaceId: process.env.CONTENTFUL_SPACE_ID!,
-      environment: process.env.CONTENTFUL_ENVIRONMENT!,
-      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN!,
-      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN!,
+      spaceId: process.env.CONTENTFUL_SPACE_ID,
+      environment: process.env.CONTENTFUL_ENVIRONMENT || 'master',
+      previewToken: process.env.CONTENTFUL_PREVIEW_TOKEN,
+      accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
     }),
   ],
   modelExtensions: [{ name: 'page', type: 'page', urlPath: '/{slug}' }]
-})
+};
+
+export default config;
